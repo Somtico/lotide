bdb40ae2f905ecc8fe876adf29ed19095280cff0
Somto Ufondu <somtoufondu@gmail.com>
2024-03-24 15:12:44 -0600

    added one more test under test case 2 to check that the tail function returns an array with one less element than the input array.

--- tail.js
+++ tail.js
@@ -28,6 +28,7 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words).length, words.length - 1); // tail should have one element less than its input
 
 // Test Case 3: An array with only one element
 assertEqual(tail([1]), 0); // ...should yield an empty array for its tail
