2a5531a94358fa631df314a60c1b187081bee96b
Somto Ufondu <somtoufondu@gmail.com>
2024-03-24 13:32:35 -0600

    created a head function to work with the assertEqual function

---
+++ head.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  // Use if...else statements to compare the two values it takes in
+  if (actual === expected) {
+    // Check if they match and print a message
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    // Else print a message
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// A function that returns the first element of an array
+const head = function(array) {
+  return array[0];
+}
+
+// TEST CODE
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([200]), "Thanks.");
+assertEqual(head([]), 5);
+assertEqual(head([true]), 5);
+assertEqual(head([false]), 5);
+
+
