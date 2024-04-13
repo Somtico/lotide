bdf12830847279ae57b30d61dfec731ed1587ce9
Somto Ufondu <somtoufondu@gmail.com>
2024-04-13 03:46:02 -0600

    applied property value shorthand syntax from ES6 to the main object being created and exported in our index.js file.

--- index.js
+++ index.js
@@ -4,16 +4,12 @@ const middle = require('./middle');
 const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
 const eqArrays = require('./eqArrays');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const eqObjects = require('./eqObjects');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  eqArrays: eqArrays,
-  assertObjectsEqual: assertObjectsEqual,
-  eqObjects: eqObjects,
+  head,
+  tail,
+  middle,
+  assertEqual,
+  assertArraysEqual,
+  eqArrays,
 };
